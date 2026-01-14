0e5c8f33da55e636f97a7b44e54a1ef06dd4e4df
phutri27 <triphu27@gmail.com>
2026-01-14 19:13:08 +0000

    rename homeController to postController and fully implement CRUD control for posts

---
+++ src/controller/postController.ts
@@ -0,0 +1,49 @@
+import { Request, Response } from "express";
+import { matchedData, validationResult } from "express-validator";
+import { blogObj } from "../queries/queries";
+import { validateBlogPublish } from "./validator/validator";
+
+export const renderPosts = async (req: Request, res: Response) => {
+    let posts
+    if (req.baseUrl == "/home") {
+        posts = await blogObj.findAllPost()
+    } else {
+        if (!req.user) {
+            return res.status(401).json({ message: "Unauthorized" })
+        }
+        posts = await blogObj.findAllPost(req.user.id) 
+    }
+    return res.status(200).json(posts) 
+}
+
+export const blogPost = [
+    ...validateBlogPublish,
+    async (req: Request , res: Response ) => {
+        const errors = validationResult(req)
+        if (!errors.isEmpty()){
+            return res.status(400).json({errors: errors.array()})
+        }
+        const {title, content, published} = matchedData(req)
+        if (req.method == 'PUT'){
+            const id = Number(req.params.id)
+            await blogObj.editPost(id, title, content,published)
+        } else {
+            if (!req.user) {
+                return res.status(401).json({ message: "Unauthorized" })
+            }
+             await blogObj.createPost(req.user.id, title, content, published)
+        }
+        return res.status(200).json({
+            message: "Post created",
+        })
+    }
+]
+
+export const deletePost = async (req: Request, res: Response) => {
+    const id = Number(req.params.id)
+    await blogObj.deletePost(id)
+    return res.status(200).json({
+        message: "Delete post successfully"
+    })
+}
+
