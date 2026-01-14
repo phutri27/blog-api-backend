0031bdcf98e968f3af7dda488d67dd51827dbb46
phutri27 <triphu27@gmail.com>
2026-01-14 15:19:12 +0000

    Create jwt using passport

---
+++ src/utils/jwt.ts
@@ -0,0 +1,21 @@
+import { ExtractJwt, Strategy as JwtStrategy, StrategyOptions} from "passport-jwt"
+import { userObj } from "../queries/queries"
+import "dotenv/config"
+
+const opts: StrategyOptions = {
+    jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+    secretOrKey: process.env.SECRET_KEY as string,
+}
+
+export const jwtSub = new JwtStrategy(opts, async (jwt_payload, done) => {
+    try{
+        console.log(jwt_payload)
+        const user = await userObj.findUserById(jwt_payload.sub)
+        if (!user){
+            return done(null, false)
+        }
+        return done(null, user)
+    } catch(err){
+        return done(err, false)
+    }
+})
